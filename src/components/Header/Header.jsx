97bc60cd99ab281d073a1fc8b3758a92493b1869
Rachel Harris <rachelharris@MacBook-Air.local>
2021-11-24 11:02:04 -0600

    Added Components folder and Header files.

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header(props) {
+    return(
+        <div className="row row-space">
+            <div className="col-md-12" style={{padding:0}}>
+                <h1>YouTube Music Library</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
