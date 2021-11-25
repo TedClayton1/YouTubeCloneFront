4b25dd74d30992fb8d079d72a2a81e09e5420d0a
Rachel Harris <rachelharris@MacBook-Air.local>
2021-11-24 19:05:35 -0600

    Added folder and files for SearchedVideos.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,13 @@ import './App.css';
 import Header from './Header/Header';
 import VideoPlayer from './VideoPlayer/VideoPlayer';
 import axios from 'axios';
+import 
 
-// setup post man with apikey
 // make a handleChange function in App.jsx
 // make a handleSubmit function in App.jsx could take the string that the user search for and pass it into the "this.getSearchResults" function
 // getSearchResults(search) search is the value we set the input to in "Header"
 // pass them into the "header" example <Header handleChange={this.handleChange} handleSubmit={this.handleSubmit} />
+
 class App extends Component{
     constructor(props){
         super(props);
@@ -18,6 +19,19 @@ class App extends Component{
             apiKey: 'AIzaSyBishejgCVBIeKnKKDganTv5dYVjB3HwCY',
         };
     }
+
+    // handleChange() {
+    //     this.setState(onChange => ({
+
+    //     }));
+    //   }
+
+    // handleSubmit()) {
+    //     this.setState(onClick => ({
+
+    //     }));
+    //   }  
+
 
     getSearchResults = async (searchTerm) => {
         let response = await axios.get(`https:www.googleapis.com/youtube/v3/search?part=snippet&q=${searchTerm}&type=video&maxResults=5&key=${this.state.key}`)
