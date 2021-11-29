46ad178baf3da09de290ac8c61d792ce73120b4d
Dustin Barker <barkerd85@yahoo.com>
2021-11-29 15:40:04 -0700

    Added description and detail to the related video function

--- src/components/RelatedVideos/RelatedVideos.jsx
+++ src/components/RelatedVideos/RelatedVideos.jsx
@@ -9,7 +9,9 @@ function RelatedVideos (props){
    
     return(
     <div>
-        {props.videos.map((element)=><><img src={element.snippet.thumbnails.default.url}/><p>{element.id.videoId}</p></>)}
+        {props.videos.map((element)=><><img src={element.snippet.thumbnails.default.url}/><p>{element.snippet.title}</p><p>{element.snippet.description}</p></>)}
+        
+        
    
     
 
